99483e31640f28977a560e6a96d601d19f18df39
bmistree <bmistree@gmail.com>
2014-11-24 10:57:18 -0800

    Adding a message listener for class for cohort messages.

--- src/java/SergeantCohort/ICohortConnection.java
+++ src/java/SergeantCohort/ICohortConnection.java
@@ -4,6 +4,19 @@ import java.io.IOException;
 
 public interface ICohortConnection
 {
-    public void add_connection_listener(ICohortConnectionListener listener);
+    /**
+       Send heartbeat message to other side.
+     */
     public void send_heartbeat(long view_number) throws IOException;
+    
+    /**
+       Listen for the connection's timing out.
+     */
+    public void add_connection_listener(ICohortConnectionListener listener);
+    
+    /**
+       Register for CohortMessage-s.  Guarantees that will receive
+       messages in order that they arrive from transport layer.
+     */
+    public void add_cohort_message_listener(ICohortMessageListener listener);
 }
---
+++ src/java/SergeantCohort/ICohortMessageListener.java
@@ -0,0 +1,15 @@
+package SergeantCohort;
+
+import ProtocolLibs.LeaderCommandProto.LeaderCommand;
+import ProtocolLibs.FollowerCommandAckProto.FollowerCommandAck;
+import ProtocolLibs.ElectionProposalProto.ElectionProposal;
+import ProtocolLibs.ElectionProposalResponseProto.ElectionProposalResponse;
+
+public interface ICohortMessageListener
+{
+    public void leader_command(LeaderCommand leader_command);
+    public void follower_command_ack(FollowerCommandAck follower_command_ack);
+    public void election_proposal(ElectionProposal election_proposal);
+    public void election_proposal_response(
+        ElectionProposalResponse election_proposal_resp);
+}
