26d88a7f5d8509b896c3a6e5da9bf3e25ef1c59c
bmistree <bmistree@gmail.com>
2014-11-30 13:53:05 -0800

    Each manager generates its own connection set.

--- src/java/SergeantCohort/CohortManager.java
+++ src/java/SergeantCohort/CohortManager.java
@@ -1,8 +1,10 @@
 package SergeantCohort;
 
 import java.util.Set;
+import java.util.HashSet;
 
 import SergeantCohort.CohortConnection.ICohortConnectionFactory;
+import SergeantCohort.CohortConnection.ICohortConnection;
 
 /**
    Makes connections to many cohorts and then tries to elect a leader
@@ -24,19 +26,27 @@ public class CohortManager implements ICohortManager
        Connection information that we should use to connect to remote
        cohort nodes.
      */
-    final private Set<CohortInfo.CohortInfoPair> connection_info;
-    final private ICohortConnectionFactory cohort_connection_factory;
-    
+    final private Set<ICohortConnection> cohort_connections =
+        new HashSet<ICohortConnection>();
+
     /**
-       @param connection_info ---
-       {@link CohortManager#connection_info}
+       @param connection_info --- Connection information that we
+       should use to connect to remote cohort nodes.
+
+       @param cohort_connection_factory --- Factory to use to generate
+       connections.
      */
     public CohortManager(
         Set<CohortInfo.CohortInfoPair> connection_info,
         ICohortConnectionFactory cohort_connection_factory)
     {
-        this.connection_info = connection_info;
-        this.cohort_connection_factory = cohort_connection_factory;
+        for (CohortInfo.CohortInfoPair pair : connection_info)
+        {
+            ICohortConnection connection =
+                cohort_connection_factory.construct(
+                    pair.local_cohort_info,pair.remote_cohort_info);
+            cohort_connections.add(connection);
+        }
     }
 
     /**
