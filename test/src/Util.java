b909ff6efd5b37e0afad03b70e817b2b67100c83
bmistree <bmistree@gmail.com>
2014-12-01 18:54:55 -0800

    Had a bug constructing cohort info sets.

--- test/src/Util.java
+++ test/src/Util.java
@@ -21,45 +21,50 @@ public class Util
     public static Map<Long,Set<CohortInfo.CohortInfoPair>>
         get_connection_info(long num_cohort_nodes)
     {
-        // keys are cohort ids; values are their local cohort info.
-        Map<Long, CohortInfo> cohort_map = new HashMap<Long,CohortInfo>();
- 
-        for (long i = 0; i < num_cohort_nodes; ++i)
-        {
-            int tcp_port = (int)(BASE_TCP_PORT + i);
-            long cohort_id = i;
-            // listen locally on all interfaces.
-            CohortInfo cohort_info = new CohortInfo (
-                "127.0.0.1",tcp_port,cohort_id);
-
-            cohort_map.put(cohort_id,cohort_info);
-        }
-
-
         Map<Long,Set<CohortInfo.CohortInfoPair>> to_return =
             new HashMap<Long,Set<CohortInfo.CohortInfoPair>>();
         for (long i = 0; i < num_cohort_nodes; ++i)
         {
-            CohortInfo local_info = cohort_map.get(i);
-            
-            Set<CohortInfo.CohortInfoPair> value =
+            Set<CohortInfo.CohortInfoPair> cohort_info_set =
                 new HashSet<CohortInfo.CohortInfoPair>();
+            to_return.put(i,cohort_info_set);
+            
+            int local_cohort_tcp_port_base =
+                BASE_TCP_PORT +  (int)((i) * num_cohort_nodes);
+            
+
             for (long j = 0; j < num_cohort_nodes; ++j)
             {
-                // do not list self in connection set
-                if (i == j)
+                // don't add a connection for self.
+                if (j == i)
                     continue;
 
-                CohortInfo remote_info = cohort_map.get(j);
+                // from i and j, determine which tcp ports each
+                // connection should be listening on.
+                int remote_cohort_tcp_port_base =
+                    BASE_TCP_PORT +  (int)((j) * num_cohort_nodes);
+
+                int local_tcp_port = local_cohort_tcp_port_base + (int)(j);
+                int remote_tcp_port = remote_cohort_tcp_port_base + (int)(i);
+
+                
+                long local_cohort_id = i;
+                long remote_cohort_id = j;
+
+
+                // create cohort information paris and add them to set.
+                CohortInfo local_info = new CohortInfo (
+                    "127.0.0.1",local_tcp_port,local_cohort_id);
+
+                CohortInfo remote_info = new CohortInfo (
+                    "127.0.0.1",remote_tcp_port,remote_cohort_id);
 
                 CohortInfo.CohortInfoPair pair =
                     new CohortInfo.CohortInfoPair(remote_info,local_info);
 
-                value.add(pair);
+                cohort_info_set.add(pair);
             }
-            to_return.put(i,value);
         }
-
         return to_return;
     }
 }
