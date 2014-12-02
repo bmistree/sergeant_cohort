778bf21fcadc2b39b0be4a46e9928495a71a2bf4
bmistree <bmistree@gmail.com>
2014-12-01 17:48:14 -0800

    Leader election test actually tries to connect and start the cohort members.  Test currentl fails (quickly).

--- test/src/LeaderElectionTest.java
+++ test/src/LeaderElectionTest.java
@@ -2,9 +2,11 @@ package test;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 
 import SergeantCohort.CohortManager;
 import SergeantCohort.CohortInfo;
+import static SergeantCohort.CohortConnection.TCPCohortConnection.TCPCohortConnectionFactory;
 
 
 /**
@@ -14,6 +16,8 @@ import SergeantCohort.CohortInfo;
 public class LeaderElectionTest
 {
     public final static long COHORT_SIZE = 3L;
+    public final static int HEARTBEAT_TIMEOUT_PERIOD_MS = 500;
+    public final static int HEARTBEAT_SEND_PERIOD_MS = 150;
     
     public static void main(String[] args)
     {
@@ -29,8 +33,43 @@ public class LeaderElectionTest
      */
     public static boolean run()
     {
+        TCPCohortConnectionFactory tcp_cohort_connection_factory =
+            new TCPCohortConnectionFactory(
+                HEARTBEAT_TIMEOUT_PERIOD_MS,HEARTBEAT_SEND_PERIOD_MS);
+
         Map<Long,Set<CohortInfo.CohortInfoPair>> cohort_map =
             Util.get_connection_info(COHORT_SIZE);
+
+        // create lots of managers
+        Set<CohortManager> cohort_managers = new HashSet<CohortManager>();
+        for (Long cohort_id : cohort_map.keySet())
+        {
+            Set<CohortInfo.CohortInfoPair> connection_info =
+                cohort_map.get(cohort_id);
+            CohortManager cohort_manager =
+                new CohortManager(
+                    connection_info,tcp_cohort_connection_factory,cohort_id);
+
+            cohort_managers.add(cohort_manager);
+        }
+        
+        // start all the managers.
+        for (CohortManager cohort_manager : cohort_managers)
+            cohort_manager.start_manager();
+
+        // wait for a while and check if we have a leader.
+        try
+        {
+            // wait five seconds
+            Thread.sleep(1000*5);
+        }
+        catch (InterruptedException ex)
+        {
+            ex.printStackTrace();
+            SergeantCohort.Util.force_assert("Some error.");
+        }
+
+        
         return true;
     }
 }
