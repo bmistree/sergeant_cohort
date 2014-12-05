047b51b8a5d5e2d1953fe68ee4f8157da4405285
bmistree <bmistree@gmail.com>
2014-12-04 16:08:37 -0800

    Added an experiment for leader flapping that actually dumps leadership changes for cohort nodes.

--- src/java/SergeantCohort/ILeaderElectedListener.java
+++ src/java/SergeantCohort/ILeaderElectedListener.java
@@ -8,5 +8,6 @@ public interface ILeaderElectedListener
     /**
        Gets called whenever enter election state.
      */
-    public void election_started(long election_view_number, long local_cohort_id);
+    public void election_started(
+        long election_view_number, long local_cohort_id);
 }
