61d5c295cd755b9da9d5edac8b65ccbd733678f3
bmistree <bmistree@gmail.com>
2014-12-09 21:12:53 -0800

    Updates graph plotting positions.

--- script/graph/plot_leader_view_number.py
+++ script/graph/plot_leader_view_number.py
@@ -12,18 +12,21 @@ NO_LEADER_ID = -1
 
 class LeaderHistoryElement(object):
     LEADER_ID_GRAPH_HEIGHTS = {
-        0: .1,
-        1: .3,
-        2: .6,
-        NO_LEADER_ID: .9
+        0: .025,
+        1: .25,
+        2: .53,
+        NO_LEADER_ID: .8
         }
+    X_LEFT_OFFSET = .03
 
     def __init__(self,view_number,leader_id):
         self.view_number = view_number
         self.leader_id = leader_id
     
     def _x_position_from_view_number(self,max_view_number):
-        return float(self.view_number) / float(max_view_number)
+        return (
+            (float(self.view_number) / float(max_view_number)) -
+            LeaderHistoryElement.X_LEFT_OFFSET)
 
     def _y_position_from_leader_id(self):
         return LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[self.leader_id]
@@ -33,7 +36,8 @@ class LeaderHistoryElement(object):
         position_y = self._y_position_from_leader_id()
         axes.annotate('X',xy=(position_x,position_y))
         
-def draw_leader_history_graph(leader_history_list, output_filename):
+def draw_leader_history_graph(leader_history_list, output_filename,
+                              title):
     '''
     @param {list} leader_history_list --- Each element is a
     LeaderHistoryElement.
@@ -44,15 +48,38 @@ def draw_leader_history_graph(leader_history_list, output_filename):
     # now actually draw graphs
     figure, axes = plt.subplots()
 
-    # figure.set_figheight(1.3)
-    # figure.set_figwidth(3)
-    # figure.subplots_adjust(top=.80,bottom=.05,left=.02,right=.98)
+    figure.set_figheight(3)
+    figure.set_figwidth(6)
+    figure.subplots_adjust(top=.90,bottom=.10,left=.02,right=.98)
     for leader_history_element in leader_history_list:
         leader_history_element.draw_on_axes(axes,max_view_number)
         
+    label_x = .05
+    label_offset_y = .07
+    no_leader_y = (
+        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[NO_LEADER_ID] +
+        label_offset_y)
+    axes.annotate('No leader',xy=(label_x,no_leader_y))
+    a_leader_y = (
+        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[0] +
+        label_offset_y)
+    axes.annotate('Node A leader',xy=(label_x,a_leader_y))
+    b_leader_y = (
+        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[1] +
+        label_offset_y)
+    axes.annotate('Node B leader',xy=(label_x,b_leader_y))
+    c_leader_y = (
+        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[2] +
+        label_offset_y)
+    axes.annotate('Node C leader',xy=(label_x,c_leader_y))
+
+    axes.set_title(title)
+    axes.set_xticks([])
+    axes.set_yticks([])        
+    axes.set_xlabel('Normalized view number')
     plt.savefig(output_filename)    
 
-def run(input_json_filename,output_filename):
+def run(input_json_filename,output_filename,title):
     '''
     @param {string} input_json_filename --- The name of a file
     containing a json list.  Each element of the json list has the
@@ -99,10 +126,12 @@ def run(input_json_filename,output_filename):
         
 
     # now actually draw the graph with filled leader_history
-    draw_leader_history_graph(filled_leader_history_list, output_filename)
+    draw_leader_history_graph(
+        filled_leader_history_list, output_filename,title)
 
 
 if __name__ == '__main__':
     input_json_filename = sys.argv[1]
     output_filename = sys.argv[2]
-    run(input_json_filename,output_filename)
+    title = sys.argv[3]
+    run(input_json_filename,output_filename,title)
