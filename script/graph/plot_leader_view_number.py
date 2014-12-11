df9186ce57417f465e7aa9e43c3a1c0f92e0eb59
bmistree <bmistree@gmail.com>
2014-12-10 22:43:29 -0800

    Added code to display differently depending on experiment run.

--- script/graph/plot_leader_view_number.py
+++ script/graph/plot_leader_view_number.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import argparse
 import sys
 import json
 
@@ -8,17 +8,38 @@ import matplotlib.lines as lines
 
 NO_LEADER_ID = -1
 
+FLAPPING_EXPERIMENT_ID_GRAPH_HEIGHTS = {
+    0: .025,
+    1: .25,
+    2: .53,
+    NO_LEADER_ID: .8
+    }
 
+RANDOM_FAILURE_EXPERIMENT_ID_GRAPH_HEIGHTS = {
+    0: .025,
+    1: .25,
+    2: .53,
+    NO_LEADER_ID: .8
+    }
+
+class GraphHeightType(object):
+    RANDOM_FAILURE_EXPERIMENT = 0
+    FLAPPING_EXPERIMENT = 1
+    
+    @staticmethod
+    def generate_choose_options():
+        return [GraphHeightType.RANDOM_FAILURE_EXPERIMENT,
+                GraphHeightType.FLAPPING_EXPERIMENT]
+
+
+def get_graph_height_dict(graph_height_type):
+    if graph_height_type == GraphHeightType.RANDOM_FAILURE_EXPERIMENT:
+        return RANDOM_FAILURE_EXPERIMENT_ID_GRAPH_HEIGHTS
+    return FLAPPING_EXPERIMENT_ID_GRAPH_HEIGHTS
 
 class LeaderHistoryElement(object):
-    LEADER_ID_GRAPH_HEIGHTS = {
-        0: .025,
-        1: .25,
-        2: .53,
-        NO_LEADER_ID: .8
-        }
     X_LEFT_OFFSET = .03
-
+    
     def __init__(self,view_number,leader_id):
         self.view_number = view_number
         self.leader_id = leader_id
@@ -28,16 +49,17 @@ class LeaderHistoryElement(object):
             (float(self.view_number) / float(max_view_number)) -
             LeaderHistoryElement.X_LEFT_OFFSET)
 
-    def _y_position_from_leader_id(self):
-        return LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[self.leader_id]
-    
-    def draw_on_axes(self,axes,max_view_number):
+    def _y_position_from_leader_id(self,graph_height_type):
+        graph_height_dict = get_graph_height_dict(graph_height_type)
+        return graph_height_dict[self.leader_id]
+
+    def draw_on_axes(self,axes,max_view_number,graph_height_type):
         position_x = self._x_position_from_view_number(max_view_number)
-        position_y = self._y_position_from_leader_id()
+        position_y = self._y_position_from_leader_id(graph_height_type)
         axes.annotate('X',xy=(position_x,position_y))
         
 def draw_leader_history_graph(leader_history_list, output_filename,
-                              title):
+                              title,graph_height_type):
     '''
     @param {list} leader_history_list --- Each element is a
     LeaderHistoryElement.
@@ -52,24 +74,26 @@ def draw_leader_history_graph(leader_history_list, output_filename,
     figure.set_figwidth(6)
     figure.subplots_adjust(top=.90,bottom=.10,left=.02,right=.98)
     for leader_history_element in leader_history_list:
-        leader_history_element.draw_on_axes(axes,max_view_number)
-        
+        leader_history_element.draw_on_axes(
+            axes,max_view_number,graph_height_type)
+
+    graph_height_dict = get_graph_height_dict(graph_height_type)
     label_x = .05
     label_offset_y = .07
     no_leader_y = (
-        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[NO_LEADER_ID] +
+        graph_height_dict[NO_LEADER_ID] +
         label_offset_y)
     axes.annotate('No leader',xy=(label_x,no_leader_y))
     a_leader_y = (
-        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[0] +
+        graph_height_dict[0] +
         label_offset_y)
     axes.annotate('Node A leader',xy=(label_x,a_leader_y))
     b_leader_y = (
-        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[1] +
+        graph_height_dict[1] +
         label_offset_y)
     axes.annotate('Node B leader',xy=(label_x,b_leader_y))
     c_leader_y = (
-        LeaderHistoryElement.LEADER_ID_GRAPH_HEIGHTS[2] +
+        graph_height_dict[2] +
         label_offset_y)
     axes.annotate('Node C leader',xy=(label_x,c_leader_y))
 
@@ -79,7 +103,7 @@ def draw_leader_history_graph(leader_history_list, output_filename,
     axes.set_xlabel('Normalized view number')
     plt.savefig(output_filename)    
 
-def run(input_json_filename,output_filename,title):
+def run(input_json_filename,output_filename,title,graph_height_type):
     '''
     @param {string} input_json_filename --- The name of a file
     containing a json list.  Each element of the json list has the
@@ -127,11 +151,32 @@ def run(input_json_filename,output_filename,title):
 
     # now actually draw the graph with filled leader_history
     draw_leader_history_graph(
-        filled_leader_history_list, output_filename,title)
+        filled_leader_history_list, output_filename,title,graph_height_type)
 
+def run_cli():
+    parser = argparse.ArgumentParser('Graph drawing code')
+    parser.add_argument(
+        '--input_json_filename',type=str,help='Path to input json file',
+        required=True)
+    parser.add_argument(
+        '--output_filename',type=str,help='Path to pdf file to generate',
+        required=True)
+    parser.add_argument(
+        '--title',type=str,help='Title to use for graph',
+        required=True)
+    parser.add_argument(
+        '--graph_height_type',type=int,
+        choices=GraphHeightType.generate_choose_options(),
+        help='Title to use for graph',
+        required=True)
+    args = parser.parse_args()
+
+    run(args.input_json_filename,args.output_filename,
+        args.title,args.graph_height_type)
 
 if __name__ == '__main__':
-    input_json_filename = sys.argv[1]
-    output_filename = sys.argv[2]
-    title = sys.argv[3]
-    run(input_json_filename,output_filename,title)
+    run_cli()
+    # input_json_filename = sys.argv[1]
+    # output_filename = sys.argv[2]
+    # title = sys.argv[3]
+    # run(input_json_filename,output_filename,title)
