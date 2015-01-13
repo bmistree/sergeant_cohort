b60f493a897e17c4579b0f52681e58c34860582e
bmistree <bmistree@gmail.com>
2015-01-13 11:29:35 -0800

    Added truncate suffix/prefix to in-memory log.  Still need to add test cases for it.

--- src/java/SergeantCohort/Storage/InMemoryList.java
+++ src/java/SergeantCohort/Storage/InMemoryList.java
@@ -8,69 +8,106 @@ import SergeantCohort.LogEntry;
 
 public class InMemoryList implements IStorage
 {
-    protected final List<LogEntry> log = new ArrayList<LogEntry>();
+    private final List<LogEntry> log = new ArrayList<LogEntry>();
+
+    /**
+       When we truncate our log from its prefix, the indices in log
+       (the ArrayList above) do not match the indices of the actual
+       log.
+
+       Eg.,
+
+           log = [1,2,3]
+           log.get(2) // == 2
+           log.truncate(2) // log == [2,3]
+           log.get(2) // should == 2.
+           
+       Therefore, we're keeping a mapping between the last index that
+       we truncated from so that we can index into log.
+
+       Eg.,
+           log.get(2)
+       turns into
+           log.get(2 - base_offset_index)
+     */
+    private long base_offset_index = 0;
     
     /**
        @see IStorage.truncate_suffix
      */
     @Override
-    public void truncate_prefix(long index_to_truncate_before)
+    public synchronized void truncate_prefix(long index_to_truncate_before)
     {
-        // FIXME: truncate_prefix should be implemented
-        Util.force_assert("FIXME: truncate_prefix is still a stub method");
+        long list_mapped_index = list_mapped_index(index_to_truncate_before);
+        for (int i = 0; i < (list_mapped_index - 1); ++i)
+            log.remove(0);
+        base_offset_index = index_to_truncate_before;
+    }
+
+
+    private synchronized long list_mapped_index (long external_index)
+    {
+        return external_index - base_offset_index;
     }
     
     /**
        @see IStorage.truncate_suffix
      */
     @Override
-    public void truncate_suffix(long index_to_truncate_after)
+    public synchronized void truncate_suffix(long index_to_truncate_after)
     {
-        // FIXME: truncate_suffix should be implemented
-        Util.force_assert("FIXME: truncate_suffix is still a stub method");
-    }
-    
-
-    @Override
-    public long log_size()
-    {
-        return (long)log.size();
+        long num_removes = log_size() - index_to_truncate_after - 1;
+        long list_mapped_index = list_mapped_index(index_to_truncate_after);
+        for (int i = 0; i < num_removes; ++i)
+            log.remove((int)(list_mapped_index + 1));
     }
 
     @Override
-    public void append_entry(byte[] contents, long term)
+    public synchronized long log_size()
+    {
+        return ((long)log.size()) + base_offset_index;
+    }
+
+    @Override
+    public synchronized void append_entry(byte[] contents, long term)
     {
         append_entry(new LogEntry(contents,term));
     }
 
     @Override
-    public void append_entry(LogEntry entry)
+    public synchronized void append_entry(LogEntry entry)
     {
         log.add(entry);
     }
     
     @Override
-    public LogEntry get_entry(long index_to_get_from)
+    public synchronized LogEntry get_entry(long index_to_get_from)
     {
-        return log.get((int)index_to_get_from);
+        long list_mapped_index = list_mapped_index(index_to_get_from);
+        return log.get((int)list_mapped_index);
     }
 
     @Override
-    public void set_entry(long index_to_set, LogEntry entry)
+    public synchronized void set_entry(long index_to_set, LogEntry entry)
     {
-        log.set((int)index_to_set,entry);
+        long list_mapped_index = list_mapped_index(index_to_set);
+        log.set((int)list_mapped_index,entry);
     }
-
+    
     @Override
-    public void set_entry(long index_to_set, byte[] contents, long term)
+    public synchronized void set_entry(
+        long index_to_set, byte[] contents, long term)
     {
+        // note: not remapping set_entry here because happens in other
+        // set_entry call.
         LogEntry entry = new LogEntry(contents,term);
         set_entry(index_to_set,entry);
     }
     
     @Override
-    public void remove_entry(long index_to_rm)
+    public synchronized void remove_entry(long index_to_rm)
     {
-        log.remove((int)index_to_rm);
+        long list_mapped_index = list_mapped_index(index_to_rm);
+        log.remove((int)list_mapped_index);
     }
 }
